77e424fe7068d29d277fac5a65e8b7db63dcda65
Rubén Cisneros <ruben.cns@gmail.com>
2024-12-07 16:31:31 +0100

    feat: conutry filter

---
+++ src/constants/countries.ts
@@ -0,0 +1,47 @@
+export const countries = [
+  {
+    name: "All",
+    code: "all",
+    icon: "🇺🇳",
+  },
+  {
+    name: "Argelia",
+    code: "arg",
+    icon: "🇩🇿",
+  },
+  {
+    name: "Egipto",
+    code: "egy",
+    icon: "🇪🇬",
+  },
+  {
+    name: "Ghana",
+    code: "gha",
+    icon: "🇬🇭",
+  },
+  {
+    name: "Marruecos",
+    code: "mrc",
+    icon: "🇲🇦",
+  },
+  {
+    name: "Nigeria",
+    code: "nga",
+    icon: "🇳🇬",
+  },
+  {
+    name: "Kenia",
+    code: "ken",
+    icon: "🇰🇪",
+  },
+  {
+    name: "Sudáfrica",
+    code: "SDF",
+    icon: "🇿🇦",
+  },
+  {
+    name: "Tanzania",
+    code: "tza",
+    icon: "🇹🇿",
+  },
+]
